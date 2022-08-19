66b2be7cc6061df4416392552306d4c0b26aca61
Steffen Markowic <steffen.markowic@posteo.de>
2022-08-19 12:38:19 +0200

    Update helper.zsh

--- helper.zsh
+++ helper.zsh
@@ -5,6 +5,12 @@ function mdl () {
     cd -
 }
 
+function ytdl () {
+    cd ~/Downloads/
+    youtube-dl $1
+    cd -
+}
+
 function phpUpload () {
     sed -i '' 's/upload_max_filesize = 300M/upload_max_filesize = 1000M/g' $1
 }
